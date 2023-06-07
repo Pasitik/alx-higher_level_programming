7a38c1918fb1b9fd766c15f57412aa8c81b795fb
Paul Asitik <paasitik@gmail.com>
2023-06-06 20:05:26 -0700

    fizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(100):
+        if (i+1) % 3 == 0 and (i+1) % 5 == 0:
+            print("FizzBuzz ", end='')
+        if (i+1) % 3 == 0:
+            print("Fizz ", end='')
+        elif (i+1) % 5 == 0:
+            print("Buzz ", end='')
+        else:
+            print(f"{i+1} ", end='')
