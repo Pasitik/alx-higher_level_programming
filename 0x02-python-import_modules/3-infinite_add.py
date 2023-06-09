8d63002d293fc5cc715de032a8edfa98829749a8
Paul Asitik <paasitik@gmail.com>
2023-06-08 22:06:35 -0700

    all

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
 
-    argLen =  len(sys.argv)
+    argLen = len(sys.argv)
 
-    add = 0 
+    add = 0
 
-    for i in range (1, argLen):
+    for i in range(1, argLen):
         add += int(sys.argv[i])
     print("{}".format(add))
