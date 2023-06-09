efe34163c240b79f88c09f8cd3fb04473fdc37b3
Paul Asitik <paasitik@gmail.com>
2023-06-08 18:10:12 -0700

    all

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,14 @@ if __name__ == "__main__":
     from sys import argv
 
     argLen = len(argv) - 1
-
+    if argLen == 0:
+        print("{} arguments.".format(argLen))
+        
     if argLen == 1:
-        print("{} argument".format(argLen))
+        print("{} argument:".format(argLen))
         print("{}: {}".format(argLen, argv[argLen]))
 
     else:
-        print("{} arguments".format(argLen))
-        if argLen > 0:
-            for i in range(1, argLen + 1):
-                print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(argLen)) 
+        for i in range(1, argLen + 1):
+            print("{}: {}".format(i, argv[i]))
