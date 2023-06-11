13e98d06b13861fcb0c66b3c1824083f236693b1
Paul Asitik <paasitik@gmail.com>
2023-06-10 18:31:22 -0700

    10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def divisible_by_2(my_list=[]):
-        return [True if i % 2 == 0 else False for i in my_list]
+def divisible_by_2(my_list=[]):
+    return [True if i % 2 == 0 else False for i in my_list]
