e778245f7b9e06c7e857c8f510803dad34e70822
Paul Asitik <paasitik@gmail.com>
2023-06-16 20:00:34 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     rom_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if isinstance(roman_string, str):
+    if isinstance(roman_string, str) and roman_string != '':
         num = rom_int.get(roman_string[len(roman_string) - 1])
         track = rom_int.get(roman_string[len(roman_string) - 1])
 
