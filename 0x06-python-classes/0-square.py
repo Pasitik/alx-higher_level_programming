e015d2800d370f100be7852c23d8142a7174b2f1
Paul Asitik <paasitik@gmail.com>
2023-06-27 20:04:24 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
+"""
+this module contains a class 'square'
+
+
+"""
+
 
 class Square:
-    pass
+    """
+    here is an empty class with no attributes or methods
 
+
+    """
+    pass
