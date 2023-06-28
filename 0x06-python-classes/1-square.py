e0403a2ba7d2890ba8e9d0ff7969fbfe80d7d100
Paul Asitik <paasitik@gmail.com>
2023-06-27 20:14:22 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+"""
+module contains class Square
+
+"""
+
+
+class Square:
+    """
+
+    this is the class for square
+
+    Attributes:
+        __size (int): this is an integer representing the length of a side
+    """
+
+    def __init__(self, size):
+        """
+        This is called when the class is initialized
+        Args:
+            size (int): this is used to initialize the __size private attribute
+        """
+        self.__size = size
