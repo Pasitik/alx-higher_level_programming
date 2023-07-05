3a80483123f01c9ad0a28c548376d0abc875ee27
Paul Asitik <paasitik@gmail.com>
2023-07-04 21:22:53 -0700

    update

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Multiply to matrices using numpy
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiply two list of lists containing integers and floats
+    Args:
+        m_a (list of lists): list of lists containing ints and floats
+        m_b (list of lists): list of lists containing ints and floats
+    Returns:
+        list of lists containing ints and floats
+    """
+    return (np.matmul(m_a, m_b))
