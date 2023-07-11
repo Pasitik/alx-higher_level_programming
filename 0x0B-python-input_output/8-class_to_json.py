a7c2c6609820c772b6be3d1e30e1b5478687de66
Paul Asitik <paasitik@gmail.com>
2023-07-11 08:27:03 -0700

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,5 +4,4 @@
 
 def class_to_json(obj):
     """Return the dictionary represntation of a simple data structure."""
-    if isinstance(obj, MyClass):
-        return obj.__dict__
+    return obj.__dict__
