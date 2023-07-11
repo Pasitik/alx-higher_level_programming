5e75bf88c105452d2104b8882d31ab8888399637
Paul Asitik <paasitik@gmail.com>
2023-07-11 06:45:05 -0700

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a function for writing into a file"""
+
+def write_file(filename="", text=""):
+    """writes string to text file (UTF8) & returns number of chars written"""
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        return f.write(text)
+
