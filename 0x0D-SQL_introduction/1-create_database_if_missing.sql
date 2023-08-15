00d04dfd6a27f7f96b1a64a52a60157f6bb39504
Paul Asitik <paasitik@gmail.com>
2023-08-15 09:44:30 -0700

    create updated

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---creates database hbtn_0c_0 if it doesnt exist
-CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+-- creates database hbtn_0c_0 if it doesnt exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
