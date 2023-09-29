5241b3e104295911437735a3a8f1b115116e420a
Paul Asitik <paasitik@gmail.com>
2023-09-29 11:05:42 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-import urllib.request
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-with urllib.request.urlopen(req) as response:
-    data = response.read()
-    utf8_content = data.decode('utf-8')
 
-print("Body response:")
-print("\t- type:", type(data))
-print("\t- content", data)
-print("\t- utf8 content", utf8_content)
+if __name__ == '__main__':
+    import urllib.request
+
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        utf8_content = data.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content", data)
+    print("\t- utf8 content", utf8_content)
