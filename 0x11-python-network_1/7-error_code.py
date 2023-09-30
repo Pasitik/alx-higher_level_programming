897a1d902600d9b61ee927d922486fc5d0395a90
Paul Asitik <paasitik@gmail.com>
2023-09-29 18:02:56 -0700

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+contains script takes in a URL,
+sends a request to the URL and displays
+the body of the response
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    res = requests.get(url)
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
